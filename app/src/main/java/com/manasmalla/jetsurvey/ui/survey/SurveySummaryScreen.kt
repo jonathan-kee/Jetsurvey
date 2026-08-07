5706aa6c17881d117521c77fa29f2386763e949d
jonathan kee <jonathankee99@gmail.com>
2026-08-07 10:38:58 +0800

    make text correct

--- app/src/main/java/com/manasmalla/jetsurvey/ui/survey/SurveySummaryScreen.kt
+++ app/src/main/java/com/manasmalla/jetsurvey/ui/survey/SurveySummaryScreen.kt
@@ -29,7 +29,7 @@ fun SurveySummaryScreen(
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-                Text("Done")
+                Text("Back")
             }
         }
     ) { paddingValues ->
